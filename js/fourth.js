8e04e0185f98d65cb3529c15f4e04142db123e78
denicccccc <goncharov.den0410@gmail.com>
2025-02-16 10:01:21 +0100

    Create fourth.js

---
+++ js/fourth.js
@@ -0,0 +1,60 @@
+// 1
+// напиши скрипт входження числа у відрізок зазначений в х1 та х2.
+// до х1
+// після х1
+// від х1 до х2
+// до х1 або після х2
+
+// const x1 = 10;
+// const x2 = 30;
+// const number = 45;
+// const lieOrNot = number < x1;
+// console.log(lieOrNot)
+// const lieOrNot2 = number > x1;
+// console.log(lieOrNot2)
+// const lieOrNot3 = number > x1 && number < x2;
+// console.log(lieOrNot3)
+// const lieOrNot4 = number < x1 || number > x2;
+// console.log(lieOrNot4)
+
+// Напиши скрипт який перевіряє можливість відкрити чат з користувачем. 
+// Для цього користувач має бути:
+// другом
+// онлайн
+// без режиму не турбувати
+
+// const isOnline = true;
+// const isFriend = true;
+// const isDnd = false;
+
+// const canOpenChat = isOnline && isFriend && !isDnd;
+// console.log(canOpenChat)
+
+// 3
+// Напиши скрипт перевірки підписки користувача під час доступу до контенту. 
+// Є три типи підпистки: free, pro та vip. Отримати доступ можуть тільки pro та vip.
+// const sub = 'free';
+// const sub = 'vip';
+
+// const canAccessContent = sub === 'vip' || sub === 'pro';
+// console.log(canAccessContent)
+1.
+// ПЕРШУ - <myFavoriteGameName> зі значенням вашої улюбленої гри(наприклад "minecraft")
+// та ДРУГУ - <myFavoriteGameText> зі значенням рядка = "my favorite game's name" 
+// 2.
+// За доромогою властивості або метода рядка 
+// отримати ОСТАННІЙ символ змінної <myFavoriteGameName>
+// та записати це значення у ТРЕТЮ змінну <lastCharacter>.
+// 3.
+// Зробити перетворення значення змінної <lastCharacter> 
+// на ВЕЛИКУ(заглавну літеру).
+// 4.
+// Вивести в консоль повідомлення, використовуючи 
+// значення ВСІХ ТРЬОХ змінних у такому форматі:
+// "The last letter in <myFavoriteGameText> "<myFavoriteGameName>" is "<lastCharacter>" !" 
+// Використовуйте шаблонний рядок!!
+// приклад рядка, що має вийти
+// The last letter in my favorite game's name "minecraft" is "T" !
+
+const myFavoriteGameName = 'real life';
+const myFavoriteGameText = "my favorite game's name";
